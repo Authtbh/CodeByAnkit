6300b587b939c8779a0be69cba2f781498dbc4a3
Ankit Rautela <ankitrautela044@gmail.com>
2024-03-21 05:08:00 +0530

    Work on Header UI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Search } from "lucide-react";
 
 const Home = () => {
   return (
     <>
-      <h1>This is Ankit Rautela.</h1>
+      <div className="header">
+        <div className="logo-section">
+          <div className="profile-image">
+            <img src="/src/assets/profile-pic.png" alt="logo" />
+          </div>
+        </div>
+
+        <div className="Button-section">
+        <form className="search-section">
+          <input
+            type="search"
+            id="query"
+            placeholder="Type here to search.."
+            aria-label="Search through site content"
+          />
+          <button>
+            <Search className="search-icon" />
+          </button>
+        </form>
+        <div className="login-signIn">
+          <button className="signUp-button">Signup</button>
+          <button className="logIn-button">Login</button>
+          
+        </div>
+        </div>
+      </div>
     </>
   );
 };
