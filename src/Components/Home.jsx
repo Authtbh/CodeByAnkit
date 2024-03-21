8185d92a99999479db9765af4948dafab7dda6f5
Ankit Rautela <ankitrautela044@gmail.com>
2024-03-21 15:44:01 +0530

    Issue fix for Profile Pic

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Search } from "lucide-react";
-import { profilePic } from "../assets/profilePic.png";
+import profilePic from "../assets/profilePic.png";
 
 const Home = () => {
   return (
